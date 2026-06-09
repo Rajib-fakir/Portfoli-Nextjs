7b3244df959162ebd6242fab2cf53ea1e65a4c32
Rajib Hasan <138379262+Rajib-fakir@users.noreply.github.com>
2026-06-09 17:07:17 +0600

    Delete src directory

--- src/components/Button.js
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export default function Button({ children, onClick, variant = 'primary', type = 'button' }) {
-  return (
-    <button type={type} onClick={onClick} className={`btn btn-${variant}`}>
-      {children}
-    </button>
-  );
-}
